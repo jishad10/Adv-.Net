e1c279f67bec588d7efa0f65bdf587b13835369a
jahidul islam <jahidilislamjishad2019@gmail.com>
2024-09-02 22:30:47 +0600

    Intro of code first

---
+++ IntroEFCF/IntroEFCF/EF/TableDesigns/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IntroEFCF.EF.TableDesigns
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("Dept")]
+        public int DeptId { get; set; }
+        public virtual Department Dept { get; set; }
+
+    }
+}
---
+++ IntroEFCF/IntroEFCF/Migrations/202409011548584_StudentForiegnKey.cs
@@ -0,0 +1,20 @@
+﻿namespace IntroEFCF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StudentForiegnKey : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Students", "DeptId");
+            AddForeignKey("dbo.Students", "DeptId", "dbo.Departments", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "DeptId", "dbo.Departments");
+            DropIndex("dbo.Students", new[] { "DeptId" });
+        }
+    }
+}

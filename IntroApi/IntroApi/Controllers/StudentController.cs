3738707d5665d2a7e659ff3e4739d8ad20164994
jahidul islam <jahidilislamjishad2019@gmail.com>
2024-09-05 00:52:19 +0600

    IntroAPI

---
+++ IntroApi/IntroApi/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using IntroApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        [HttpGet]
+        [Route("api/student/all")]
+        public HttpResponseMessage AllStudents()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "data");
+        }
+        [HttpGet]
+        [Route("api/student/{id}")]
+        public HttpResponseMessage SingleStudent(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, " Single sudent " + id);
+        }
+
+        [HttpPost]
+        [Route("api/student/create")]
+        public HttpResponseMessage CreateStudent(Student s)
+        {
+            //db object
+            //db.Students.Add(s);
+            //db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, "Created");
+
+        }
+    }
+}

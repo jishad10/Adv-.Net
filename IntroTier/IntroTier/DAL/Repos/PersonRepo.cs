982b5ff060649bc088310252b44695d842220a32
jahidul islam <jahidilislamjishad2019@gmail.com>
2024-09-07 23:24:53 +0600

    Practecing Layer Model

---
+++ IntroTier/IntroTier/DAL/Repos/PersonRepo.cs
@@ -0,0 +1,46 @@
+﻿using DAL.EF;
+using DAL.EF.TableModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DAL.Repos
+{
+    public class PersonRepo
+    {
+        static Context db = new Context();
+        public static List<Person> Get()
+        {
+            return db.Persons.ToList();
+        }
+        public static Person Get(int id)
+        {
+            //var person = db.Persons.SingleOrDefault(x=> x.Id == id);
+            return db.Persons.Find(id);
+        }
+        public static bool Create(Person p)
+        {
+            db.Persons.Add(p);
+            return db.SaveChanges() > 0;
+        }
+        public static bool Update(Person p)
+        {
+            var exobj = Get(p.Id);
+            db.Entry(exobj).CurrentValues.SetValues(p);
+            return db.SaveChanges() > 0;
+        }
+        public static bool Delete(int id)
+        {
+            var exobj = Get(id);
+            db.Persons.Remove(exobj);
+            return db.SaveChanges() > 0;
+        }
+
+
+    }
+}
